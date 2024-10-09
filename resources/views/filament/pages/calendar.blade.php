349788f5c2a9c54ec00d7abfa39b724c7ce42524
Rafael de Macêdo <r4faelmf@gmail.com>
2024-10-09 15:45:49 -0300

    fix styles

--- resources/views/filament/pages/calendar.blade.php
+++ resources/views/filament/pages/calendar.blade.php
@@ -1,5 +1,6 @@
 <x-filament-panels::page>
-    <x-filament::input.wrapper>
+
+    <x-filament::input.wrapper class="w-1/3">
         <x-filament::input.select wire:model.live="selectedRoom">
             <option value="">Todas as salas</option>
             @foreach($rooms as $id => $name)
