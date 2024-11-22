d4ad07f606a9cf73d3d2388ab883a580d8ecba56
Rafael de Macêdo <r4faelmf@gmail.com>
2024-11-22 12:31:51 -0300

    correção da formatação da data na mensagem de validação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        Validator::replacer('after_or_equal', function ($message, $attribute, $rule, $parameters) {
+            // Formata a data para "d/m/Y"
+            $date = \Carbon\Carbon::createFromFormat('Y-m-d', $parameters[0])->format('d/m/Y');
+            return str_replace(':date', $date, $message);
+        });
     }
 }
